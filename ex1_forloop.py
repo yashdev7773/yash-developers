35d716b341ceeab81267e8865a527e5364ac8f3c
Yash.Dev <1yashwantsingh@gmail.com>
2025-04-05 21:35:39 +0530

    3rd commit with code edit

---
+++ ex1_forloop.py
@@ -0,0 +1,14 @@
+# Count to n and print each number
+n = int(input("Enter the value of n: "))
+def count_to_n(x = n):
+for i in range(1, x + 1):
+print(i, end=' ')
+print()
+
+print("Going to count to {} ... ".format(n))
+count_to_n(n)
+#-
+print("first modification for 2nd commit")
+print("Modification for the 4th commit")
+
+print("Added to see checkout is safe"
---
+++ ex2_dfuntion.py
@@ -0,0 +1,16 @@
+def double(n):
+return 2 * n
+#Call the function with the value of your choice and print its result
+print("To Call this function using its name and assign the value of your")
+You can run the Python interpreter in the directory where you use
+and then import the module by its name For ex: import simple_funct.
+and then call the function inside module like simple_function1.doul
+you can also assign the value obtained from this function to a new
+your choice for ex: x=simple_function1.double(5), and then print t
+#x = double(3)
+#print(x)
+print("The program ran successfully")
+#
+print("first modification for 2nd commit")
+#
+print("Added to see checkout is safe")
